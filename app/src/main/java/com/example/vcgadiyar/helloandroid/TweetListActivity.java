6de0e613dc47c00fb5928e244d99264d08f4761c
Vignesh Gadiyar <vcgadiyar@Vigneshs-MacBook-Pro.local>
2016-01-16 18:43:55 -0800

    first commit on master

---
+++ app/src/main/java/com/example/vcgadiyar/helloandroid/TweetListActivity.java
@@ -0,0 +1,15 @@
+package com.example.vcgadiyar.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TweetListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_list);
+    }
+}
